2d83fd75b719709b0086cd01e606192ed15955e6
basekat <dgk@tropot.net>
2021-03-26 09:10:35 +0100

    submission Bite 46 @ codechalleng.es

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,5 +1,5 @@
 def fizzbuzz(num):
-    return ', '.join(['Fizz' if x % 3 == 0
+    return ', '.join(['Fizz Buzz' if x % 15 == 0
+                      else 'Fizz' if x % 3 == 0
                       else 'Buzz' if x % 5 == 0
-                      else 'Fizz Buzz' if x % 15 == 0
                       else str(x) for x in range(1, num)])
